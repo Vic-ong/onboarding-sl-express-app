d08e96f620d17cde8ea8806d0e06f470e92408d8
Vic Ong <vickyocc53@gmail.com>
2019-05-22 16:40:07 +0800

    🐳 add publisher and consumer role

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const { App: SuperApp } = require('@shopline/sl-express');
 
 class Server extends SuperApp {
-    async connectAwesomeService() {
-        await AwesomeService.sharedAwesomeService
-            .init(this.config.awesomeService)
-            .connect();
-    }
+    // async connectAwesomeService() {
+    //     await AwesomeService.sharedAwesomeService
+    //         .init(this.config.awesomeService)
+    //         .connect();
+    // }
 
-    async disconnectAwesomeService() {
-        await AwesomeService.SharedAwesomeService.disconnect();
-    }
-
-    async connectDependencies() {
-        await super.connectDependencies();
-        // await this.connectAwesomeService();
-    }
-
-    async disconnectDependencies() {
-        // await this.disconnectAwesomeService();
-        await super.disconnectDependencies();
-    }
+    // async disconnectAwesomeService() {
+    //     await AwesomeService.SharedAwesomeService.disconnect();
+    // }
 
     async startService() {
         if (this.config.app.role == 'CONSUMER') {
